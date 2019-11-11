326112e2d40808a44db3177356d7e2bd53b7b946
Austin Johnson <technicallyaustin@gmail.com>
2019-11-11 23:27:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
 array1 = ["I","am","really","learning"]
-  array1 << "arrays"
+  array1 << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
